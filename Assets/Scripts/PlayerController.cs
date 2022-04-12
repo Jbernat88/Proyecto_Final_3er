dce65e2ceed1777d6281dd2693ba43483c57561d
Jbernat88 <91455587+Jbernat88@users.noreply.github.com>
2022-04-12 09:46:43 +0200

    fdddddd

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,9 +9,13 @@ public class PlayerController : MonoBehaviour
     public float turnspeed = 40f;
     private float horizontalInput;
     private float verticalInput;
-    public float jumpForce = 400f;
+    public float jumpForce = 200f;
 
     public bool isOnGround;
+    public bool lookRight = true;
+    public bool lookLeft;
+    public bool doubleJump = true;
+
 
     private Rigidbody playerRigidbody;
 
@@ -28,6 +32,9 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+
+        Debug.Log("Salto" +doubleJump);
+        Debug.Log("Ground"+isOnGround);
         // Usamos los inputs del Input Manager
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
@@ -36,23 +43,48 @@ public class PlayerController : MonoBehaviour
         //transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
 
 
-        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput, Space.World);
 
-        if (isOnGround == true)
+        
+        
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
+            if(isOnGround == true && doubleJump == true)
+            {               
                 playerRigidbody.AddForce(Vector3.up * jumpForce);
                 isOnGround = false;
-
-                speed = 10f;
-
+                doubleJump = true;
+                speed = 5f;
                 StartCoroutine(CoolDown());
             }
+            else if (doubleJump == true)
+            {
+                playerRigidbody.AddForce(Vector3.up * jumpForce);
+                doubleJump = false;
+            }            
+
         }
-         
+              
+        
+
+        if( horizontalInput < 0 && lookRight==true)
+        {
+            transform.Rotate(new Vector3(0, 180, 0));
+            lookRight = false;
+            lookLeft = true;
+        }
+
+        if (horizontalInput > 0 && lookLeft == true)
+        {
+
+            transform.Rotate(new Vector3(0, 180, 0));
+            lookLeft = false;
+            lookRight = true;
+        }
+
+
         //float horizontalInput = Input.GetAxis("Horizontal");
         //playerRigidbody.AddTorque(Vector3.up * turnspeed * horizontalInput);
 
@@ -74,6 +106,7 @@ public class PlayerController : MonoBehaviour
         if (otherCollider.gameObject.CompareTag("ground"))
         {
             isOnGround = true;
+            doubleJump = true;
 
             speed = 20f;
         }
